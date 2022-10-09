f01ec6d1d01bdb5d208fe2c0c6001e4fe02e83d7
Mirsaid <mirsaidalizade6@gmail.com>
2022-10-09 22:33:42 +0000

    push configs

---
+++ 1-nodeapp-ubuntu/README.md
@@ -0,0 +1,1 @@
+creating image from scratch
---
+++ 3-node-server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+
+});
+
+app.listen(port, () => {
+
+  console.log(`Example app listening at https://localhost:$(port)`);
+
+});
+
+
